bfad0e2a93589292546f81032d496c4a360ae842
davlatbek <shahriyorolimov4@gmail.com>
2024-03-16 17:18:03 +0700

    end

--- src/routes/bulimlar/Bulimlar.jsx
+++ src/routes/bulimlar/Bulimlar.jsx
@@ -1,7 +1,37 @@
 import "./Bulimlar.css";
+import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
+import { FaArrowRight } from "react-icons/fa6";
+
 
 const Bulimlar = () => {
-  return <div>Bulimlar</div>;
+  return <div className="container">
+    <h1 className="h11">Bo'limlar</h1>
+    <div className="card">
+      <div className="cards"> <img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" /> <h2 className="h21" ><center>Shoshilinch</center> <center>qabul</center></h2> <div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div> </div>
+      <div className="cards"><img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" /> <h2 className="h21" ><center>Bolalar bo`limi</center></h2> <div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div></div>
+      <div className="cards"> <img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" /> <h2 className="h21" ><center>Tug`ruq bo`limi</center></h2><div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div> </div>
+      <div className="cards"> <img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" /> <h2 className="h21" ><center>Ginekologiya</center> <center>bo`limi</center></h2> <div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div> </div>
+    </div>
+    <div className="card">
+      <div className="cards1"> <img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" />  <h2 className="h211" > <center>Homiladorlar</center> <center>xastaligi bo`limi</center> </h2><div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div></div>
+      <div className="cards1"><img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" /> <h2 className="h211" ><center>Jarohatlarni</center>  <center>davolash</center><center>bo`limi</center> </h2><div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div> </div>
+      <div className="cards1"> <img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" /> <h2 className="h211" > <center>Fizioterapiya</center> <center>bo`limi</center></h2> <div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div></div>
+      <div className="cards1"> <img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" /> <h2 className="h211" > <center>Yuqumli</center> <center>kasalliklar</center> <center>bo`limi</center></h2> <div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div></div>
+    </div>
+    <div className="card">
+      <div className="cards2"> <img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" />  <h2 className="h211" > <center>Terapiya bo`limi</center> </h2><div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div></div>
+      <div className="cards2"><img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" /> <h2 className="h211" ><center>Laboratoriya</center><center>bo`limi</center> </h2><div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div></div>
+      <div className="cards2"> <img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" /> <h2 className="h211" > <center>Tez tibbiy</center> <center>yordam bo`limi</center></h2><div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div> </div>
+      <div className="cards2"> <img src="https://api.narpay-ttb.uz/media/services/1_zcwaBR9.jpg" alt="" className="imgs" /> <h2 className="h211" > <center>Reablitatsiya</center><center>bo`limi</center></h2><div className="hideElement"> <h2>Batafsil <FaArrowRight className="icon"/></h2> </div> </div>
+    </div>
+    <div className="rq">
+      <h1> <AiOutlineDoubleLeft className="rqqq" /></h1>
+      <h1 className="rqq"> <center>1</center></h1>
+      <h1 className="rqq"> <center>2</center></h1>
+      <h1 className="rqq"> <center>3</center></h1>
+      <h1> <AiOutlineDoubleRight className="rqqq" /></h1>
+    </div>
+  </div>
 };
 
 export default Bulimlar;
