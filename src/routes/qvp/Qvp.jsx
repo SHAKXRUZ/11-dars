58ef738a88692d7f88ea9b75c31e8be285e4c0ef
Sa1dakbar09 <sa1dakbarnu777@gmail.com>
2024-03-16 15:40:28 +0500

    qvp full

--- src/routes/qvp/Qvp.jsx
+++ src/routes/qvp/Qvp.jsx
@@ -1,7 +1,342 @@
 import "./Qvp.css";
 
 const Qvp = () => {
-  return <div>Qvp</div>;
+  return <div>
+
+    <div className="result">
+
+      <div className="item box">
+        <div className="one">
+          <h2>№</h2>
+        </div>
+        <div className="two">
+          <h2>Manzil</h2>
+        </div>
+        <div className="three">
+          <h2>Biriktirilgan shifokor</h2>
+        </div>
+        <div className="four">
+          <h2>Telefon</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>1</h2>
+        </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika</h2>
+        </div>
+        <div className="three">
+          <h2>Xudoyberdiyev Avazbek Egamberdiyevich</h2>
+        </div>
+        <div className="four">
+          <h2>+998901912812</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>2</h2>
+        </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika Bobur filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Qurbonova Sarvinoz Nasriddinovna</h2>
+        </div>
+        <div className="four">
+          <h2>+998943567609</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>3</h2>
+        </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika Oytamg`ali filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Eshqobilova Hakima Ergashovna</h2>
+        </div>
+        <div className="four">
+          <h2>+998939337735</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>4</h2>
+        </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika Narpay filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Usmanov Oybek Imom O`g`li</h2>
+        </div>
+        <div className="four">
+          <h2>+998999537531</h2>
+        </div>
+      </div>
+
+      <div className="item"><div className="one">
+        <h2>5</h2>
+      </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika Mushkent filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Xudoyberdiyeva Fazolat Arslonovna</h2>
+        </div>
+        <div className="four">
+          <h2>+998991312544</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>6</h2>
+        </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika O`zbekiston filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Artiqova Zebiniso Toshquvvatovna</h2>
+        </div>
+        <div className="four">
+          <h2>+998940126556</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>7</h2>
+        </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika Yangiobod filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Hojiyev Sa`dulla Nazarovich</h2>
+        </div>
+        <div className="four">
+          <h2>+998995410958</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>8</h2>
+        </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika Mirbozor filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Muhammadiyev G`olib Ismoilovich</h2>
+        </div>
+        <div className="four">
+          <h2>+998979163072</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>9</h2>
+        </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika Oqtosh filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Hikmatova Iroda Murodullayeva</h2>
+        </div>
+        <div className="four">
+          <h2>+998939638776</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>10</h2>
+        </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika Islom Shoir filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Namozova Sayyora Sattorovna</h2>
+        </div>
+        <div className="four">
+          <h2>+998944069718</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>11</h2>
+        </div>
+        <div className="two">
+          <h2>Ko`p tarmoqli markaziy poliklinika Kosogoron filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Hushmurodova Muborak Rajabovna</h2>
+        </div>
+        <div className="four">
+          <h2>+998972861967</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>12</h2>
+        </div>
+        <div className="two">
+          <h2>13-oilaviy poliklinika</h2>
+        </div>
+        <div className="three">
+          <h2>Rahimova Gulondon Mamaniyozovna</h2>
+        </div>
+        <div className="four">
+          <h2>+998997537369</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>13</h2>
+        </div>
+        <div className="two">
+          <h2>13-oilaviy poliklinika Haqiqat filiali</h2>
+        </div>
+        <div className="three">
+          <h2>O`tkir Umarov Sattorovich</h2>
+        </div>
+        <div className="four">
+          <h2>+998944768560</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>14</h2>
+        </div>
+        <div className="two">
+          <h2>13-oilaviy poliklinika Ulug`bek filiali</h2>
+        </div>
+        <div className="three">
+          <h2>Bekov Davlatbek Shodiyevich</h2>
+        </div>
+        <div className="four">
+          <h2>+998938517773</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>15</h2>
+        </div>
+        <div className="two">
+          <h2>Samarqand Oilaviy shifokorlik punkti</h2>
+        </div>
+        <div className="three">
+          <h2>Odilova Munisa Davron qizi</h2>
+        </div>
+        <div className="four">
+          <h2>+998996858546</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>16</h2>
+        </div>
+        <div className="two">
+          <h2>Oq oltin Oilaviy shifokorlik punkti</h2>
+        </div>
+        <div className="three">
+          <h2>Murodov Lutfulla Xayrullayevich</h2>
+        </div>
+        <div className="four">
+          <h2>+998933455206</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>17</h2>
+        </div>
+        <div className="two">
+          <h2>Urganji Oilaviy shifokorlik punkti</h2>
+        </div>
+        <div className="three">
+          <h2>Suyarova Dilbar Izzatullayevna</h2>
+        </div>
+        <div className="four">
+          <h2>+998994196325</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>18</h2>
+        </div>
+        <div className="two">
+          <h2>Yulduz Oilaviy shifokorlik punkti</h2>
+        </div>
+        <div className="three">
+          <h2>Muxtorova Buvigul Yarqulovna</h2>
+        </div>
+        <div className="four">
+          <h2>+998942403188</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>19</h2>
+        </div>
+        <div className="two">
+          <h2>Xo`jaqo`rg`on Oilaviy shifokorlik punkti</h2>
+        </div>
+        <div className="three">
+          <h2>Shamsiyev Sherzod Shokirovich</h2>
+        </div>
+        <div className="four">
+          <h2>+998996697035</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>20</h2>
+        </div>
+        <div className="two">
+          <h2>Alisher Navoiy Oilaviy shifokorlik punkti</h2>
+        </div>
+        <div className="three">
+          <h2>Toshova Mahbuba Ergashovna</h2>
+        </div>
+        <div className="four">
+          <h2>+998915514686</h2>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="one">
+          <h2>21</h2>
+        </div>
+        <div className="two">
+          <h2>Murtazo Shopiyev Oilaviy shifokorlik punkti</h2>
+        </div>
+        <div className="three">
+          <h2>Ablayev</h2>
+        </div>
+        <div className="four">
+          <h2>+998991620232</h2>
+        </div>
+      </div>
+
+    </div>
+
+  </div>;
 };
 
 export default Qvp;
